8bace085b0af25c4102a539df57afa80099366c9
app2641 <app2641@gmail.com>
2016-05-26 23:23:41 +0900

    Increase match pattern

--- tests/runner_test.js
+++ tests/runner_test.js
@@ -68,6 +68,7 @@ QUnit.test('Runner', function (assert) {
   msgTest('user1', '昨日は１９：００に帰りました', ['@user1 1/1 19:00 退勤']);
   msgTest('user1', ':frog:', ['@user1 1/2 12:34 退勤']);
   msgTest('user1', 'get wild', ['@user1 1/2 12:34 退勤']);
+  msgTest('user1', 'さよなら', ['@user1 1/2 12:34 退勤']);
 
   msgTest('user1', '休憩した', ['@user1 1/2 1:00 休憩']);
   msgTest('user1', 'ランチ 3:00', ['@user1 1/2 3:00 休憩']);
@@ -76,4 +77,5 @@ QUnit.test('Runner', function (assert) {
   msgTest('user1', '12月はこれ https://docs.google.com/spreadsheets/d/1Iz7dh8OqseD3SaxhSHW_ZWIK_vRwNq5I--osPSnFTEM/edit#gid=1245018676', []);
   msgTest('app2641', '12月はこれ https://docs.google.com/spreadsheets/d/1Iz7dh8OqseD3SaxhSHW_ZWIK_vRwNq5I--osPSnFTEM/edit#gid=1245018676', ['@app2641 12月の業務日報を登録']);
   msgTest('user1', '12月の日報', ['@user1 https://docs.google.com/spreadsheets/d/spreadsheet_id/edit']);
+  msgTest('user1', '12月の業務日報', ['@user1 https://docs.google.com/spreadsheets/d/spreadsheet_id/edit']);
 });
